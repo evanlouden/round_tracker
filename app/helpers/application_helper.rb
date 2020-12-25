4b87ba041e829a4d6038778f856520c8fd1c6b3b
Evan Louden <evanlouden@gmail.com>
2020-12-25 17:15:49 -0500

    New add button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,21 @@ module ApplicationHelper
     end
     "#{course.name[0..space_index - 1]}<br />#{course.name[space_index + 1..-1]}".html_safe
   end
+
+  def embedded_svg(filename, options={})
+    file = File.read(Rails.root.join("app", "assets", "images", filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css("svg")
+    if options[:class].present?
+      svg["class"] = options[:class]
+    end
+
+    if svg.children.present?
+      svg.children.each do |child|
+        child["stroke"] = options[:stroke] if options[:stroke]
+        child["fill"] = options[:fill] if options[:fill]
+      end
+    end
+    doc.to_html.html_safe
+  end
 end
