d5e489d53793adc04e5e6b6ab16a6f9d4b44d26f
Evan Louden <evanlouden@gmail.com>
2020-12-27 21:20:13 -0500

    update tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,6 @@
-let environment = {
-  plugins: [
-    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
-};
-
-if (process.env.RAILS_ENV === 'production') {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        "./app/**/*.html.erb"
-      ],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
-
-module.exports = environment;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './app/views/*.html.erb',
+  ],  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
