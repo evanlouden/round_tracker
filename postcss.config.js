bb1f0343c0684e03d4efb1471dd3ebafe20dbcc8
Evan Louden <evanlouden@gmail.com>
2020-12-29 22:34:37 -0500

    production purge 🤞

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let environment = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
@@ -12,3 +12,17 @@ module.exports = {
     })
   ]
 }
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment
