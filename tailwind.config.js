fc8cb926aa07401f27dadf8719e3213ba0f8d00d
Evan Louden <evanlouden@gmail.com>
2021-09-03 10:05:17 -0400

    feat: Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/javascripts/**/*.js'
+  ],
   darkMode: false, // or 'media' or 'class'
   plugins: [],
 }
