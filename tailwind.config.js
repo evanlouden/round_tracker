426753eb8e0fa4d83053e406e04ff7dd91e3c96f
Evan Louden <evanlouden@gmail.com>
2022-07-02 21:29:46 -0400

    chore: Update to css-bundling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './app/**/*.html.erb',
+  content: [
+    './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/assets/javascripts/**/*.js',
-    './config/initializers/simple_form.rb'
-  ],
-  darkMode: false, // or 'media' or 'class'
-  plugins: [],
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
 }
