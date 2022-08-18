0e94199566ec6fc6ba9b3f9e456173fd778f25ef
Evan Louden <evanlouden@gmail.com>
2022-08-17 23:23:33 -0400

    Add email to user (including encryption)

---
+++ db/migrate/20220818022512_change_user_name_to_email.rb
@@ -0,0 +1,6 @@
+class ChangeUserNameToEmail < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :email, :string
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2021_07_17_131943) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_022512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2021_07_17_131943) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
